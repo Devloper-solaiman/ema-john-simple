de66b81c3feae416ba6943b60625cc07190dd577
devsolaiman <devlopersolaiman@gmail.com>
2022-04-05 16:12:52 +0600

    added a cart

---
+++ src/conponents/Cart/Cart.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Cart = ({cart}) => {
+    return (
+        <div>
+              <h4>Order summary</h4>
+                <p>Selected Items:{cart.length} </p>
+                <p>Total Price:${} </p>
+                <p>Total Shipping charge:${} </p>
+                <p>Tax:${} </p>
+        </div>
+    );
+};
+
+export default Cart;
